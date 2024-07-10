dac869af704108ced0efe1c66dbbd5654bdfb611
franklynekirwa2 <sangkirwa2@gmail.com>
2024-07-10 11:13:23 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+
+function findMatching(drivers, searchTerm) {
+
+   const searchTermLower = searchTerm.toLowerCase();
+
+   return drivers.filter(driver => {
+     const driverLower = driver.toLowerCase();
+     return driverLower.includes(searchTermLower);
+   });
+ }
+
+ function fuzzyMatch(drivers, searchTerm) {
+
+   const searchTermLower = searchTerm.toLowerCase();
+
+   return drivers.filter(driver => {
+     const driverLower = driver.toLowerCase();
+     return driverLower.startsWith(searchTermLower);
+   });
+ }
+  function matchName(){
+
+  }
+
+function matchName(drivers, searchTerm) {
+  const searchTermLower = searchTerm.toLowerCase();
+    return drivers.filter(driver => {
+    return driver.name === searchTerm
+  });
+}
